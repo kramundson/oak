cff2ee6fc71bfd539dd00a2f001ac2e387524f2c
Lisa Malins <lkmalins@ucdavis.edu>
2020-01-31 17:31:31 -0800

    gap_finder.py now dies politely if wrong number of arguments or if genome file doesn't exist

--- scripts/gap_finder.py
+++ scripts/gap_finder.py
@@ -1,9 +1,19 @@
 import sys
+import os
 from re import finditer
 from Bio import SeqIO
 from Bio.Seq import Seq
 
-usage = "python gap_finder.py genome.fa n_windows.bed"
+usage = "Usage: python gap_finder.py genome.fa > n_windows.bed\n"
+if len(sys.argv) < 2:
+    sys.stderr.write("Need to specify a genome file.\n{}".format(usage))
+    sys.exit(1)
+if len(sys.argv) > 2:
+    sys.stderr.write("Too many arguments.\n{}".format(usage))
+    sys.exit(1)
+if not os.path.isfile(sys.argv[1]):
+    sys.stderr.write("File {} not found.\n{}".format(sys.argv[1], usage))
+    sys.exit(1)
 
 for record in SeqIO.parse(sys.argv[1], "fasta"):
     s = str(record.seq)
