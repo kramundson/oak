6411722c969585b8b340f5b47f0650465bc8335b
Lisa Malins <lkmalins@ucdavis.edu>
2020-02-05 17:01:12 -0800

    In gap_finder.py, added option to exclude gaps shorter than a minimum number of bases

--- scripts/gap_finder.py
+++ scripts/gap_finder.py
@@ -7,15 +7,27 @@ usage = "Usage: python gap_finder.py genome.fa > n_windows.bed\n"
 if len(sys.argv) < 2:
     sys.stderr.write("Need to specify a genome file.\n{}".format(usage))
     sys.exit(1)
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     sys.stderr.write("Too many arguments.\n{}".format(usage))
     sys.exit(1)
 if not os.path.isfile(sys.argv[1]):
     sys.stderr.write("File {} not found.\n{}".format(sys.argv[1], usage))
     sys.exit(1)
 
+try:
+    min_gap_length = sys.argv[2]
+except IndexError:
+    min_gap_length = str(1)
+
+sys.stderr.write("Finding gaps of minimum length {} in file {}\n".format(min_gap_length, sys.argv[1]))
+
+gap_count = 0
+
 for record in SeqIO.parse(sys.argv[1], "fasta"):
     s = str(record.seq)
 
-    for gap in finditer("N+", s):
+    for gap in finditer("N{{{},}}".format(min_gap_length), s):
         print(record.id, gap.start(), gap.end(), sep="\t")
+        gap_count += 1
+
+sys.stderr.write("Total gaps found: {}\n".format(gap_count))
